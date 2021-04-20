d5aac5784219ff84b9882974fadcf5f55a3c04f3
Matteo Lecca <matteolecca@MacBookdiMatteo.home-life.hub>
2021-04-20 10:26:18 +0200

    first commit

--- src/routers/orders-router.js
+++ src/routers/orders-router.js
@@ -1,31 +1,6 @@
 const express = require ('express')
 const router = new express.Router()
 const dbAsync = require ('../db/asyncDB')
-const cors = require('cors')
-var jwt = require('jsonwebtoken');
-const { calculatePrice } = require('../helper/priceCalculator')
-// {
-//     order: {
-//       email: 'matteolecca00@gmail.com',
-//       name: 'matteo',
-//       surname: 'lecca',
-//       city: 'cagliari',
-//       postcode: '09124',
-//       address: 'via sassari 49'
-//     },
-//     products: [
-//       {
-//         ID: 204,
-//         name: 't-shirt',
-//         price: 45.5,
-//         image: '//cdn.shopify.com/s/files/1/0323/5826/8972/products/TT-White-NEW-2-800x1035_1000x.progressive.jpg?v=',
-//         description: 'T-shirt customized',
-//         color: '',
-//         category: 't-shirt',
-//         size: 'xs',
-//         quantity: 1
-//       }
-//     ]
 
 
 router.post('/order', async (req,res)=>{
@@ -62,12 +37,6 @@ router.get('/calculateprice', async (req,res)=>{
     
     
 })
-// router.post('/deleteorder', async (req,res) =>{
-//     const result = await dbAsync.deleteOrder(req.body.ID)
-//     if(result.error) return res.status(400).send(result.error)
-//     return res.send()
-   
-// })
 
 // router.get('/orders/:token', async (req,res) =>{
 //     const token = req.params.token
